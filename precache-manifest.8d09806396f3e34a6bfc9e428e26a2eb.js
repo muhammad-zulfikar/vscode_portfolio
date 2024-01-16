cb49fa2f559d0e289cf610a2abb335f6410724e1
Muhammad Zulfikar <155657912+muhammad-zulfikar@users.noreply.github.com>
2024-01-16 22:04:03 +0000

    Updates

---
+++ precache-manifest.8d09806396f3e34a6bfc9e428e26a2eb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "530cbfec85ee87950f6295961600fbdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac26b67ee8f6430979fe",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "ac26b67ee8f6430979fe",
+    "url": "/static/js/2.b0f3fb47.chunk.js"
+  },
+  {
+    "revision": "9edf093c9b2dd581ee4f86aacfe93f97",
+    "url": "/static/js/2.b0f3fb47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc1b6848da0932e3a398",
+    "url": "/static/js/3.2b82b7df.chunk.js"
+  },
+  {
+    "revision": "8874006ccd1b8f35999a",
+    "url": "/static/js/4.4ab5a10d.chunk.js"
+  },
+  {
+    "revision": "0158596eb79cb16ed3ea",
+    "url": "/static/js/5.6f271fab.chunk.js"
+  },
+  {
+    "revision": "7dbd44174c238deeb4be",
+    "url": "/static/js/6.e898ef9b.chunk.js"
+  },
+  {
+    "revision": "9aebf78c5fa8eb2d0c4a",
+    "url": "/static/js/main.22cd961a.chunk.js"
+  },
+  {
+    "revision": "3b5d372bba9400000cc9",
+    "url": "/static/js/runtime-main.514bd313.js"
+  },
+  {
+    "revision": "e35473ec0cf2c0d1a583440c3c3a99e1",
+    "url": "/static/media/education.e35473ec.md"
+  },
+  {
+    "revision": "a8c44302b6dffd0296d87ff7b032e014",
+    "url": "/static/media/experience.a8c44302.md"
+  },
+  {
+    "revision": "c6ce8f37884c11a1160c1aa2cf9e93dc",
+    "url": "/static/media/projects.c6ce8f37.md"
+  },
+  {
+    "revision": "6f5b2950de3b67f2d5baccac70fc5e28",
+    "url": "/static/media/skills.6f5b2950.md"
+  }
+]);
